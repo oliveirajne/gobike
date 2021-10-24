cc0cdd801296943165813ef5c9bba6b2386fe81c
Jayne <oliveirajne@gmail.com>
2021-10-24 00:44:09 -0300

    home de busca

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import React from "react"
+import styles from '../estilos/styles.module.css';
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.container_mapa}>
+                <div className={styles.container_gradiente}>
+                    <div className={styles.container_bottom}>
+                        <div className={styles.container_buttons}>
+                            <div>
+                                <input type="text" placeholder="Local de partida" />
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Destino" />
+                            </div>
+                            <div>
+                                <input type="submit" value="ENCONTRAR TRAJETO" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
